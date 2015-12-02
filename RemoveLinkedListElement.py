0ef3ae3b39c0de798a6677c3542aea51039f3750
lingcheng99 <lingcheng99@gmail.com>
2015-12-02 15:38:57 -0800

    Update RemoveLinkedListElement.py

--- RemoveLinkedListElement.py
+++ RemoveLinkedListElement.py
@@ -1,10 +1,12 @@
-"""Remove Linked List Elements
+"""
+Remove Linked List Elements
 Remove all elements from a linked list of integers that have value val.
 
 Example
 Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5 
 """
+
 class ListNode(object):
      def __init__(self, x):
          self.val = x
