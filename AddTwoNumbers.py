08fb7042106c9f42604d32aebe998422f7d84d2e
lingcheng99 <lingcheng99@gmail.com>
2015-12-04 19:07:27 -0800

    Update AddTwoNumbers.py

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,4 +1,4 @@
-""
+"""
 Add Two Numbers
 You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -20,25 +20,16 @@ class Solution(object):
         """
         if not l1 and not l2:
             return None
-        if not l1:
-            return l2
-        if not l2:
-            return l1
         
-        num1=l1.val
-        i=1
-        l1=l1.next
+        num1,num2,i,j=0,0,0,0
         while l1:
             num1=(10**i)*l1.val+num1
             l1=l1.next
             i+=1
-        num2=l2.val
-        i=1
-        l2=l2.next
         while l2:
-            num2=(10**i)*l2.val+num2
+            num2=(10**j)*l2.val+num2
             l2=l2.next
-            i+=1
+            j+=1
         
         num=num1+num2
         s=str(num)[::-1]
@@ -47,7 +38,7 @@ class Solution(object):
         for i in range(1,len(s)):
             pos.next=ListNode(int(s[i]))
             pos=pos.next
-        pos.next=None
+        
         return head
 
 #Second solution,with dummy head,extra for increase in digit,and nodes to combine two list
@@ -70,6 +61,6 @@ class Solution(object):
                 extra=1
             else:
                 extra=0
-        pos.next=None
+        
         return dummy.next
 
